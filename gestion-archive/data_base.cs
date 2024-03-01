07e0f6944673e7d7c9424f8d436a0a0bec5b8b75
thomas-cad <t.cadegros@outlook.fr>
2024-03-01 19:51:45 +0100

    Back End : Connection to the BDD + Authentification of the user

---
+++ gestion-archive/data_base.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+
+namespace data_base
+{
+    public class Data_base
+    {
+        // Connection Informations for DB
+        private static string Host = "***REMOVED***";
+        private static string User = "***REMOVED***";
+        private static string Password = "***REMOVED***";
+        private static string Port = "***REMOVED***";
+
+        //Connection to the DB
+        public static NpgsqlConnection GetDBConnection(string DBname)
+        {
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            NpgsqlConnection conn = new NpgsqlConnection(connString);
+
+            return conn;
+        }
+    }
+}
---
+++ gestion-archive/password_ft.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace password
+{
+    public class PasswordHasher
+    {
+        // Method to Hash a Password
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        // Compare a password
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string hashedInput = HashPassword(password);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashedInput, hashedPassword) == 0;
+        }
+    }
+}
